f0ed8cd110b9a83de5e7036a26998888c61dbb09
vtretyakov <vtretyakov@synapticon.com>
2018-02-11 15:47:18 +0100

    fill in filter init method

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -24,6 +24,31 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
+    
+    //Set the number of particles
+    num_particles = 10; //start with some not too high number and later tune
+    
+    default_random_engine gen;
+
+    //Create normal distributions for x, y and theta
+    normal_distribution<double> dist_x(x, std[0]);//std_x
+    normal_distribution<double> dist_y(y, std[1]);//std_y
+    normal_distribution<double> dist_theta(theta, std[2]);//std_theta
+    
+    for (int i = 0; i < num_particles; i++){
+        
+        Particle particle;
+        particle.id = i;
+        particle.x = dist_x(gen);
+        particle.y = dist_y(gen);
+        particle.theta = dist_theta(gen);
+        particle.weight = 1;
+        
+        particles.push_back(particle);
+
+    }
+    
+    is_initialized = true;
 
 }
 
