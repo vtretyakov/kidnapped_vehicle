2c9f4fa34606802bf9ea57d2dd1af1f73412d2c6
vtretyakov <vtretyakov@synapticon.com>
2018-02-11 16:21:52 +0100

    fill in filter prediction method

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -57,6 +57,34 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
+    
+    default_random_engine gen;
+    
+    // Distributions for adding random Gaussian noise
+    normal_distribution<double> dist_x(0, std_pos[0]);
+    normal_distribution<double> dist_y(0, std_pos[1]);
+    normal_distribution<double> dist_theta(0, std_pos[2]);
+    
+    for (int i = 0; i < num_particles; i++){
+    
+        //Update x, y and the yaw angle
+        if (fabs(yaw_rate) < 0.0001) {//avoid devision by zero
+            particles[i].x += velocity * delta_t * cos(particles[i].theta);
+            particles[i].y += velocity * delta_t * sin(particles[i].theta);
+            //do not update theta due to no yaw rate
+        } else {
+            double new_theta = particles[i].theta + yaw_rate * delta_t;
+            particles[i].x += (velocity / yaw_rate) * (sin(new_theta) - sin(particles[i].theta));
+            particles[i].y += (velocity / yaw_rate) * (cos(particles[i].theta) - cos(new_theta));
+            particles[i].theta = new_theta;
+        }
+        
+        //Add Gaussian noise
+        particles[i].x += dist_x(gen);
+        particles[i].y += dist_y(gen);
+        particles[i].theta += dist_theta(gen);
+    
+    }
 
 }
 
